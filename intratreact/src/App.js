6bb8219c5ae3309a77dce90b576d0f9c01409c96
Yan Zhou <zhouyan87910@gmail.com>
2022-10-24 09:35:18 -0400

    commit init

---
+++ intratreact/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+function App() {
+  return (
+      <BrowserRouter>
+        <Routes>
+        </Routes>
+      </BrowserRouter>
+  );
+}
+
+export default App;
